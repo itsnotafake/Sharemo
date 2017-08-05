4c5fede23360fa6de4bccc26931a17bcaeeb6464
Devin <itsnotafake@gmail.com>
2017-08-05 15:19:31 -0700

    feat: android init

---
+++ SharemoAndroid/app/src/main/java/n7/sharemoandroid/ShareActivity.java
@@ -0,0 +1,10 @@
+package n7.sharemoandroid;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Devin on 8/5/2017.
+ */
+
+public class ShareActivity extends AppCompatActivity {
+}
